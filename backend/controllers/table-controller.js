e32cd2e001022d3428a2b35e9772cb90ae929096
Khalid <dev.y2mi@gmail.com>
2023-12-11 09:26:23 +0300

    update system function for validating reservations

--- backend/controllers/table-controller.js
+++ backend/controllers/table-controller.js
@@ -2,6 +2,7 @@ import database from "../models/index.js";
 
 /// * [connect to database and use {Menu Table}]
 const Tables = database.tables;
+const Reservations = database.reservations;
 
 /**
  * @GET_ALL_TABLES
@@ -30,5 +31,50 @@ const getAllTables = async (req, res, next) =>{
     }
 };
 
+const test = async (req, res, next)=>{
+    try {
+        //Get All tables from DB
+        const tables = await Tables.findAll();
 
-export default {getAllTables};
+        //Check if there is no tables throw error
+        if(!tables){
+            let error = new Error("Table reservation isn't avaialable at this moment!");
+            error.status = 404;
+            throw error;
+        }
+        
+        const reservations = await Reservations.findAll();
+
+        for(let table of tables){
+            for(let res of reservations){
+                if(table.id == res.table_id){
+                    table.availability = "Unavailable";
+                }
+            }
+        }
+
+        for (let table of tables) {
+            for (let reservation of reservations) {
+                if (table.id == reservation.table_id) {
+                    table.availability = "Unavailable";
+                    reservation.status = "Confirmed";
+
+                    // Save changes to the Reservations table
+                    await reservation.save();
+                }
+            }
+        }
+        
+        //Create object from Date -> to generate current request date
+        // const date = new Date();
+        // NowDate: `${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`}
+        //Successful request will return response with all tables with OK! status
+        res.send(tables).status(200);
+    } catch (err) {
+        console.log("Table-Controller:\nMethod: getAllTables-Error.\n" + err.message);
+        next(err);
+    }
+};
+
+
+export default {getAllTables, test};
--- backend/routes/tables.js
+++ backend/routes/tables.js
@@ -2,6 +2,7 @@ import express from 'express';
 import tableController from '../controllers/table-controller.js';
 const router = express.Router();
 
-router.get('/', tableController.getAllTables);
+// router.get('/', tableController.getAllTables);
+router.get('/', tableController.test);
 
 export default router;
